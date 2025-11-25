9b4e51a526298f4b74ae5603763a88ca1fe259c7
Luis Muñoz <luismugor@gmail.com>
2025-11-25 14:14:11 +0100

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/norm-coord-ptel/',
+  base: '/normalizador-geolocalizador-ptel/',
   plugins: [
     react(),
     tailwindcss(),
